f67946bd574e1d0361a090eedd7c1e0dc9044679
Nemanja <nemanja.nm123@gmail.com>
2026-08-23 15:18:40 +0200

    Add register transaction for creating organization

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../shared/prisma.js";
 
-export async function registerUser(email: string, password: string, name: string) {
+export async function registerUser(email: string, password: string, name: string, organizationName: string) {
     const existingUser = await prisma.user.findUnique({ where: { email } });
 
     if(existingUser) {
@@ -10,8 +10,37 @@ export async function registerUser(email: string, password: string, name: string
 
     const passwordHash = await bcrypt.hash(password, 10);
 
-    return prisma.user.create({ 
-        data: { email, passwordHash, name },
-    });
-    
+    const slug = organizationName.toLowerCase().trim().replace(/\s+/g, "-");
+
+    const result = await prisma.$transaction(async (tx) => {
+        const user = await tx.user.create({
+            data: {
+                email,
+                passwordHash,
+                name,
+            },
+        });
+
+        const organization = await tx.organization.create({
+            data: {
+                name: organizationName,
+                slug,
+            },
+        });
+
+        await tx.orgMembership.create({
+            data: {
+                organizationId: organization.id,
+                userId: user.id,
+                role: "OWNER",
+            },
+        });
+
+        return { user, organization };
+    })
+
+    return result;
+
 }
+
+ 
