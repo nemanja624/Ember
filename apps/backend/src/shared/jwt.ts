6badffa471e2135e7a6d79b4ffb0386e5608416c
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-08-27 18:11:50 +0200

    Fix import statement for OrgRole type

--- apps/backend/src/shared/jwt.ts
+++ apps/backend/src/shared/jwt.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import type { OrgRole } from "@prisma/client";
+import { OrgRole } from "@prisma/client";
 
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
