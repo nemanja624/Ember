987f6a2bbeaf68985df061f9e1ac95870df9f22b
Nemanja <nemanja.nm123@gmail.com>
2026-09-03 17:11:40 +0200

    Fix typo

--- apps/backend/src/auth/auth.routes.ts
+++ apps/backend/src/auth/auth.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import type { Router as ExpressRouter } from "express";
 import { login, getUserInfo, register, refresh, logout } from "./auth.controller.js";
 import { authMiddleware } from "./auth.middleware.js";
+import { requireRole } from "./auth.requireRole.js";
 
 export const authRouter: ExpressRouter = Router();
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.get("/me", authMiddleware, getUserInfo);
 authRouter.post("/refresh", refresh);
-authRouter.post("/loguot", logout);
+authRouter.post("/logout", logout);
